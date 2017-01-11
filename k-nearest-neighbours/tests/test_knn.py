89654859b89c165aead374987f5461d0a1e3f4f3
Vineet John <vinjohn10@gmail.com>
2017-01-10 22:48:55 -0500

    Refined knn class. Added k-fold to test case

--- k-nearest-neighbours/tests/test_knn.py
+++ k-nearest-neighbours/tests/test_knn.py
@@ -1,4 +1,7 @@
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+import numpy as np
+
+from utils.knn import Knn
 
 data_file_path = "/home/v2john/cs698_code/cs698-assignments/k-nearest-neighbours/data/data1.csv"
 label_file_path = "/home/v2john/cs698_code/cs698-assignments/k-nearest-neighbours/data/labels1.csv"
@@ -18,8 +21,29 @@ with open(label_file_path) as label_file:
         label = line.strip()
         labels.append(label)
 
-print("There are " + str(len(data_vectors)) + " data vectors")
-print("There are " + str(len(labels)) + " labels")
 
+# X_train, X_test, y_train, y_test = train_test_split(data_vectors, labels, test_size=0.1, random_state=42)
+#
+# knn = Knn(X_train, X_test, y_train, y_test, 3)
+# test_set_predicted_y = knn.get_test_set_predictions()
+# print test_set_predicted_y
+# print y_test
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+kf = KFold(n_splits=10)
+
+total = 0
+correct = 0
+for train_index, test_index in kf.split(data_vectors):
+    X_train, X_test = np.array(data_vectors)[train_index], np.array(data_vectors)[test_index]
+    y_train, y_test = np.array(labels)[train_index], np.array(labels)[test_index]
+
+    knn = Knn()
+    knn.fit(X_train, y_train)
+    predicted_y = knn.predict(X_test, 4)
+
+    for i in xrange(len(predicted_y)):
+        if predicted_y[i] == y_test[i]:
+            correct += 1
+        total += 1
+
+print "accuracy: " + str(correct * 1.0/total)
--- k-nearest-neighbours/utils/knn.py
+++ k-nearest-neighbours/utils/knn.py
@@ -5,48 +5,55 @@ import sys
 
 class Knn(object):
 
-    def __init__(self, train_set_x, test_set_x, train_set_y, test_set_y, k):
-        self.train_set_x = train_set_x
-        self.test_set_x = test_set_x
-        self.train_set_y = train_set_y
-        self.test_set_y = test_set_y
-        self.k = k
+    def __init__(self):
+        self.train_set_x = None
+        self.train_set_y = None
 
-    def get_test_set_predictions(self):
+    def fit(self, train_set_x, train_set_y):
+        self.train_set_x = train_set_x.tolist()
+        self.train_set_y = train_set_y.tolist()
 
-        for i in xrange(len(self.test_set_x)):
+    def predict(self, test_set_x, k):
+        test_set_x = test_set_x.tolist()
+        test_set_predicted_y = list()
+        for i in xrange(len(test_set_x)):
+
+            # dictionary containing mapping of distances of test_set[i] to a list of indices in the train_set
             dist_dict = dict()
 
+            # calculate distance vs each point in the train set, and add it to the dictionary
             for j in xrange(len(self.train_set_x)):
-                dist = distance.euclidean(self.test_set_x[i], self.train_set_x[j])
-                prev_list = dist_dict[dist]
-                if prev_list:
+                dist = distance.euclidean(test_set_x[i], self.train_set_x[j])
+                prev_list = None
+                if dist in dist_dict:
+                    prev_list = dist_dict[dist]
                     prev_list.append(j)
                 else:
                     prev_list = [j]
                 dist_dict[dist] = prev_list
 
-            final_point_indices = []
-            sorted_distances = sorted(dist_dict.keys())
+            # print dist_dict
 
+            # getting the nearest k options by finding the minimum distances in dist_dict
+            final_point_indices = list()
+            sorted_distances = sorted(dist_dict.keys())
             i = 0
-            while len(final_point_indices) < self.k:
-                list_indices = sorted_distances[i]
+            while len(final_point_indices) < k:
+                list_indices = dist_dict[sorted_distances[i]]
                 final_point_indices.extend(list_indices)
                 i += 1
+            final_point_indices = final_point_indices[:k]
 
-            final_point_indices = final_point_indices[:self.k]
-
+            # counting frequency of each label
             label_count_dict = dict()
             for index in final_point_indices:
                 value = self.train_set_y[index]
-
-                count = label_count_dict[value]
-                if count:
+                if value in label_count_dict.keys():
                     label_count_dict[value] += 1
                 else:
                     label_count_dict[value] = 1
 
+            # identifying the common label
             common_label = None
             highest_frequency = -sys.maxsize
             for value in label_count_dict.keys():
@@ -54,6 +61,6 @@ class Knn(object):
                     highest_frequency = label_count_dict[value]
                     common_label = value
 
-            test_set_predicted_y = list()
-            test_set_predicted_y[i] = common_label
+            test_set_predicted_y.append(common_label)
 
+        return test_set_predicted_y
