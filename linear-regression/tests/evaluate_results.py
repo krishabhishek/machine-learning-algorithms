fc1eeb2a26ec908ce1ae50ca57af9abe9da1950c
Vineet John <vinjohn10@gmail.com>
2017-01-13 15:59:34 -0500

    Added evaluation framework for linear regression

---
+++ linear-regression/tests/evaluate_results.py
@@ -0,0 +1,26 @@
+import json
+
+import matplotlib.pyplot as plt
+
+results_file_path = \
+    "/home/v2john/Dropbox/Personal/Academic/Masters/UWaterloo/" \
+    "Academics/Winter2017/cs698/assignments/A1/results_crossval.json"
+
+with open(results_file_path) as results_file:
+    results = json.load(results_file)
+
+
+x_values = list()
+y_values = list()
+
+for k in results.keys():
+    x_values.append(k)
+    y_values.append(results[k])
+
+plt.xlabel('k')
+plt.ylabel('Accuracy')
+plt.title('KNN Accuracy Graph')
+plt.plot(x_values, y_values, 'ro')
+plt.axis([0, 30, 0, 1])
+
+plt.show()
