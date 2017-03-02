c40711fcc872581e31c62ebf46ab4061e0268dba
Vineet John <vinjohn10@gmail.com>
2017-03-01 19:48:27 -0500

    Updated results. Modified evaluate results code

--- gaussian-process-regression/tests/evaluate_results_gaussian_kernel.py
+++ gaussian-process-regression/tests/evaluate_results_gaussian_kernel.py
@@ -3,7 +3,8 @@ import json
 import matplotlib.pyplot as plt
 
 results_file_path = \
-    "/home/v2john/results_linear_reg.json"
+    "/home/v2john/Projects/machine-learning-algorithms/assignments/a3/results/" \
+    "results_gaussian_process_reg_identity.json"
 
 with open(results_file_path) as results_file:
     results = json.load(results_file)
@@ -15,18 +16,19 @@ y_values = list()
 
 y_min = 10000
 best_lambda = None
+
 for k in results.keys():
     x_values.append(k)
-    y_values.append(results[k])
+    y_values.append(results[k][''])
 
     if results[k] < y_min:
         y_min = results[k]
         best_lambda = k
 
 
-plt.xlabel('Lambda')
+plt.xlabel('Gaussian - Standard Deviation')
 plt.ylabel('Error - Euclidean loss')
-plt.title('Linear Regression Error Graph\n Best Lambda = ' + str(best_lambda))
+plt.title('Gaussian Process Regression Error Graph\n Best Kernel Standard Deviation = ' + str(best_lambda))
 plt.plot(x_values, y_values)
 
 # plt.axis([0, 40, 0, 4])
