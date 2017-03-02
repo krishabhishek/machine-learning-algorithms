17eec99a59af24c6132b372bca2fe6c9e7f4798c
Vineet John <vinjohn10@gmail.com>
2017-03-02 17:31:25 -0500

    Correct error metric

--- bayesian-generalized-linear-regression/utils/metrics_helper.py
+++ bayesian-generalized-linear-regression/utils/metrics_helper.py
@@ -6,4 +6,4 @@ def calculate_euclidean_loss(predictions, target):
         squared_error = pow(diff, 2)
         sum_of_squared_errors += squared_error
 
-    return sum_of_squared_errors / 2
+    return sum_of_squared_errors / len(predictions)
--- gaussian-process-regression/tests/evaluate_results_gaussian_kernel.py
+++ gaussian-process-regression/tests/evaluate_results_gaussian_kernel.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 results_file_path = \
     "/home/v2john/Projects/machine-learning-algorithms/assignments/a3/results/" \
-    "results_gaussian_process_reg_identity.json"
+    "results_gaussian_process_reg_gaussian.json"
 
 with open(results_file_path) as results_file:
     results = json.load(results_file)
@@ -15,22 +15,47 @@ x_values = list()
 y_values = list()
 
 y_min = 10000
-best_lambda = None
+best_stddev = None
 
 for k in results.keys():
     x_values.append(k)
-    y_values.append(results[k][''])
+    y_values.append(results[k]['error'])
 
-    if results[k] < y_min:
-        y_min = results[k]
-        best_lambda = k
+    if results[k]['error'] < y_min:
+        y_min = results[k]['error']
+        best_stddev = k
 
-
+plt.figure(0)
 plt.xlabel('Gaussian - Standard Deviation')
 plt.ylabel('Error - Euclidean loss')
-plt.title('Gaussian Process Regression Error Graph\n Best Kernel Standard Deviation = ' + str(best_lambda))
-plt.plot(x_values, y_values)
+plt.title('Gaussian Process Regression Error Graph (Gaussian Kernel) \n Best Kernel Standard Deviation = ' + str(best_stddev))
+plt.plot(x_values, y_values, 'bx:')
 
-# plt.axis([0, 40, 0, 4])
+plt.axis([None, None, 0, None])
+
+plt.show()
+
+############################
+
+
+x_values = list()
+y_values = list()
+y_min = 10000
+
+for k in results.keys():
+    x_values.append(k)
+    y_values.append(results[k]['elapsed_time'])
+
+    if results[k]['elapsed_time'] < y_min:
+        y_min = results[k]['elapsed_time']
+        best_stddev = k
+
+plt.figure(1)
+plt.xlabel('Gaussian - Standard Deviation')
+plt.ylabel('Computational Time (seconds)')
+plt.title('Gaussian Process Regression - Computational Time (Gaussian Kernel)')
+plt.plot(x_values, y_values, 'bx:')
+
+plt.axis([None, None, 0, 10])
 
 plt.show()
