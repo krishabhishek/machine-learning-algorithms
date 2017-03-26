fed0ee1e725ad1eee7c7ade236f56b05be5f1dd6
Vineet John <vinjohn10@gmail.com>
2017-03-25 23:25:16 -0400

    Added softmax and cnn image classification processors

--- tensorflow-mnist/scripts/run-tensorflow-mnist.sh
+++ tensorflow-mnist/scripts/run-tensorflow-mnist.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 CODEDIR=$(dirname "$0")"/../"
-DATADIR=$(dirname "$0")"/../data/"
 
 # Run Gaussian Mixture Model processor
 /usr/bin/python3 "$CODEDIR"/tensorflow_mnist.py
--- tensorflow-mnist/tensorflow_mnist.py
+++ tensorflow-mnist/tensorflow_mnist.py
@@ -2,18 +2,28 @@ from argparse import ArgumentParser
 
 import sys
 
-from processors.mnist_processor import MnistProcessor
+from processors.cnn_processor import CNNProcessor
+from processors.softmax_regression_processor import SoftmaxRegressionProcessor
 from utils.options import Options
 
 
 def main(argv):
+    processor = None
     options = parse_args(argv)
-    processor = MnistProcessor(options)
-    processor.process()
+
+    if options.mode == "softmax":
+        processor = SoftmaxRegressionProcessor(options)
+    elif options.mode == "cnn":
+        processor = CNNProcessor(options)
+
+    if processor:
+        processor.process()
 
 
 def parse_args(argv):
     parser = ArgumentParser(prog="tensorflow_mnist")
+    parser.add_argument('--mode', metavar='Run mode', type=str, required=True)
+
     return parser.parse_args(argv, namespace=Options)
 
 
