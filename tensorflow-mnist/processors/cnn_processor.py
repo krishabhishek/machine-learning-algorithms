bd5f1c9e8c63ccbbbc853ee1398ea6c622da2443
Vineet John <vinjohn10@gmail.com>
2017-03-31 14:02:00 -0400

    Added discussion results for a5

--- tensorflow-mnist/processors/cnn_processor.py
+++ tensorflow-mnist/processors/cnn_processor.py
@@ -59,13 +59,21 @@ class CNNProcessor(Processor):
             log.info("Training iteration: " + str(i))
             batch = mnist.train.next_batch(50)
             if i % 100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={
-                    x: batch[0], y_: batch[1], keep_prob: 1.0})
+                train_accuracy = accuracy.eval(
+                    feed_dict={
+                        x: batch[0], y_: batch[1], keep_prob: 1.0
+                    }
+                )
                 log.info("step %d, training accuracy %g" % (i, train_accuracy))
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-        log.info("test accuracy %g" % accuracy.eval(feed_dict={
-            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+        log.info(
+            "test accuracy %g" % accuracy.eval(
+                feed_dict={
+                    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0
+                }
+            )
+        )
 
         log.info("CNNProcessor concluded")
 
